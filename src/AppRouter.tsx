84a7e957a8daac62988fad86f38b97322621b821
Fran Lopez <12franciscoinfor41c@gmail.com>
2025-02-05 13:39:39 +0100

    Quitado el env de la publi

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,14 +1,14 @@
 import { lazy } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-const WeatherInfo = lazy(() => import('./components/layout'));
+const Layout = lazy(() => import('./components/layout'));
 // import TimelogManager from './views/Timelog/TimelogManager';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" index element={<WeatherInfo />} />
+        <Route path="/" index element={<Layout />} />
 
         {/*<Route path="/login" element={<LoginPage />} /> */}
 
